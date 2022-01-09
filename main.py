0e87ed6b91892600e0a64e0a4ffe109f2de6387f
Oliwn <om.strauss@outlook.com>
2022-01-09 17:54:51 +0100

    Saving information in a csv-file, cleaning up the code and adding comments

--- main.py
+++ main.py
@@ -1,32 +1,26 @@
-import Bio
-from Bio import Entrez
-from Bio import SeqIO
+#Link to github-repository: https://github.com/Oliwn/phylogenetics.git
+from Bio import Entrez, SeqIO, Phylo, Seq
 from Bio.SeqUtils import GC
-from Bio.Align import MultipleSeqAlignment, PairwiseAligner
-from Bio import AlignIO
-from Bio.Align.Applications import ClustalwCommandline
-from Bio import pairwise2
-from Bio.pairwise2 import format_alignment
-from Bio import Phylo
-from Bio.Phylo.TreeConstruction import DistanceCalculator
-from Bio import Seq
-from Bio.Phylo.TreeConstruction import DistanceTreeConstructor
-import matplotlib
-import matplotlib.pyplot as plt
+from Bio.Align import MultipleSeqAlignment
+from Bio.Phylo.TreeConstruction import DistanceCalculator, DistanceTreeConstructor
 from Bio.SeqUtils.ProtParam import ProteinAnalysis
 
+#If not stated otherwise, I found most of the commands in this "documentation": https://biopython.org/docs/1.76/api/Bio.html
+#I worked on this program together with Theresa Plumer and Gralf Antiga in a call and we helped each other out when needed,
+#so our code will show some similarities.
+
 Entrez.email = "be19b066@technikum-wien.at"
-#handle = Entrez.einfo(db="gene")
 
 #I got the following 5 lines from: https://stackoverflow.com/questions/13557981/getting-a-gene-sequence-from-entrez-using-biopython
+#It searches for the gene name and saves the found IDs for further search
 GENE_NAME = "KCNT1"
 handle = Entrez.esearch(db="nucleotide", term=GENE_NAME, retmax=100)
 records = Entrez.read(handle)
 handle.close()
 ids = records["IdList"]
-#testprint
-print(ids)
 
+#Searching the DB for the found IDs. The sequence gets saved if the organism of the sequence has not already been found
+print("Searching for sequences...")
 orthologs = []
 organisms = []
 for i in range(len(ids)):
@@ -38,19 +32,14 @@ for i in range(len(ids)):
     if not organism in organisms:
         orthologs.append(records)
         organisms.append(organism)
-        print(organism)
+        print("Found a sequence in organism: " + organism)
     if len(orthologs) == 5:
         break
-
-
-print(orthologs)
-
-
 
 GCs = []
 for rec in orthologs:
     GCs.append(GC(rec.seq))
-print(GCs)
+print("Calculated GCs")
 
 #Since the sequences are not the same length, the shorter ones get padded with dots
 #Source: https://stackoverflow.com/questions/32833230/biopython-alignio-valueerror-says-strings-must-be-same-length
@@ -60,54 +49,37 @@ for rec in orthologs:
     if len(rec.seq) != maxlen:
         sequence = str(rec.seq).ljust(maxlen, ".")
         rec.seq = Seq.Seq(sequence)
-print(orthologs)
 align = MultipleSeqAlignment(orthologs)
-
-#AlignIO.write(align, "alignments.phy", "clustal")
-
-#aligner = PairwiseAligner()
-#alignments = []
-# for i in range(len(orthologs)):
-#     j = i + 1
-#     while j <= 4:
-#         print(str(i) + " " + str(j))
-#         alignment = aligner.align(orthologs[i].seq, orthologs[j].seq)
-#         AlignIO.write(alignment, "alignments.phy", "phylip")
-#         alignments.append(alignment[0])
-#         j += 1
-
-
-#with open("alignments.phy", "r") as aln:
-#    alignment = AlignIO.read(aln, "clustal")
-#print(type(alignment))
+print("Sequences aligned")
 
 #I used the following video and the tutorial which is linked in the description for creating the tree: https://www.youtube.com/watch?v=wBdz3vFQ4Ks
-
 calculator = DistanceCalculator("identity")
 distanceMatrix = calculator.get_distance(align)
-#print(distanceMatrix)
-
 constructor = DistanceTreeConstructor(calculator)
 tree = constructor.build_tree(align)
-#print(tree)
-
 fig = Phylo.draw(tree)
 
+#The nucleotide sequences are translated and saved. Other parameters are calculated and partially saved.
 proteins = []
 for rec in orthologs:
-    proteins.append(rec.seq.transcribe().translate(to_stop=True))
-print("--------------------")
+    proteins.append(rec.seq.translate(to_stop=True))
+print("Translating the nucletoide sequences in protein ones...")
+arom = []
+instab = []
 for pro in proteins:
-    print(str(pro))
+    print("Seq: " + str(pro))
     temp = ProteinAnalysis(str(pro))
-    print(temp.aromaticity())
-    print(temp.instability_index())
-    print(temp.isoelectric_point())
-    print(temp.secondary_structure_fraction())
+    arom.append(temp.aromaticity())
+    instab.append(temp.instability_index())
+    print("Aromaticity: " + str(temp.aromaticity()))
+    print("Instability: " + str(temp.instability_index()))
+    print("Isoelectric point: " + str(temp.isoelectric_point()))
+    print("Secondary structure fraction: " + str(temp.secondary_structure_fraction()) + "\n")
 
+#The required parameters are written into a csv-file
+with open("orthologs.csv", "w") as f:
+    f.write("Accession number,Organism,Length,GC%,Instability,Aromaticity\n")
+    for i in range(len(orthologs)):
+        f.write(orthologs[i].name + "," + orthologs[i].annotations["organism"] + "," + str(len(orthologs[i].seq)) + "," + str(GCs[i]) + "," + str(instab[i]) + "," + str(arom[i]) + "\n")
 
-#calculator = DistanceCalculator("identity")
-#distanceMatrix = calculator.get_distance(alignments)
-#print(distanceMatrix)
-#alignments = pairwise2.align.globalxx(list(orthologs.values())[0].seq, list(orthologs.values())[1].seq)
-#print(format_alignment(*alignments[0]))
+print("\nSaved information in 'orthologs.csv'")
